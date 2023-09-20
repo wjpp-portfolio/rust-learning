1e51594f1b9d9d76cd9028f8aba56cf12b7f6781
Will <will@wiri.co.uk>
2023-09-20 15:53:43 +0100

    hello world

---
+++ rust_book/hello_world/target/debug/deps/hello_world-0af4c503821086f2.d
@@ -0,0 +1,5 @@
+/home/will/Documents/Python Projects/rust-learning/rust_book/hello_world/target/debug/deps/hello_world-0af4c503821086f2.rmeta: src/main.rs
+
+/home/will/Documents/Python Projects/rust-learning/rust_book/hello_world/target/debug/deps/hello_world-0af4c503821086f2.d: src/main.rs
+
+src/main.rs:
---
+++ rust_book/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/will/Documents/Python\ Projects/rust-learning/rust_book/hello_world/target/debug/hello_world: /home/will/Documents/Python\ Projects/rust-learning/rust_book/hello_world/src/main.rs
